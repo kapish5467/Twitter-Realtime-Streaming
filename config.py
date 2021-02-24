71881e120efb77f63fd160963673d85296c55ef7
Kapish Garg <kapish5467@gmail.com>
2021-02-23 19:32:46 -0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-access_token= '1169235812950495232-a1P3V6t6daKDvGB8SBMox4FpaIE2xy'
-access_secret='J716sRfPUCgK8DRPJPdbzAbnNhZSEbIeZWeLul0d11mg9'
-consumer_key='urKHmPV9Hw6rv788My5AYazqH'
-consumer_secret='09xUe9dV2kAmd8euX3nLc4zHmVm004R78zSkplm0uKA3Rb0vSv'
-location_API_key='fad736454f584bba8fea91ddbd85de1e'
-#APIkey='PgmQNXgwvVHZfA472hcs7Zydb'
-#APisecretkey='F8Iebuqz5dNDFqilnqtAB0olHuzbofizAM1eOvvXojDjS3nObG'
-#Bearer token='AAAAAAAAAAAAAAAAAAAAAKTnMAEAAAAAA5HuFjHv6VC0f%2FF1GBlUYccLMTc%3DMqi5nU2wYhOF4zR9YBITLrbS31WQb1VRlMnHksZX2vZQmDoRXd'
